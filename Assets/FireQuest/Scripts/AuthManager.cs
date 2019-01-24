fa56ae4835f7dd0cfa800915f95bacacd89e55d6
Emanuel Lucas de Sousa e Silva <cardgameheritage@gmail.com>
2019-01-23 22:03:19 -0300

    auth with email and password

---
+++ Assets/FireQuest/Scripts/AuthManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+
+public class AuthManager : MonoBehaviour {
+
+	Firebase.Auth.FirebaseAuth auth;
+	public delegate IEnumerator AuthCallback(Task<Firebase.Auth.FirebaseUser> task, string operation);
+	public event AuthCallback authCallback;
+
+	private void Awake()
+	{
+		auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+	}
+
+	public void SignUpNewUser(string email, string password)
+	{
+		auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+			StartCoroutine(authCallback(task, "sign_up"));
+		});
+	}
+
+	public void LogInExistingUser(string email, string password)
+	{
+		auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+			StartCoroutine(authCallback(task, "login"));
+		});
+	}
+}
---
+++ Assets/FireQuest/Scripts/FormManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Firebase;
+using Firebase.Auth;
+
+public class FormManager : MonoBehaviour {
+
+	// UI objects linked from the inspector
+	public InputField emailInput;
+	public InputField passwordInput;
+
+	public Button signUpButton;
+	public Button loginButton;
+
+	public Text statusText;
+
+	public AuthManager authManager;
+
+	void Awake() {
+		ToggleButtonStates (false);
+		authManager.authCallback += HandleAuthCallback;
+	}
+
+	/// <summary>
+	/// Validates the email input
+	/// </summary>
+	public void ValidateEmail() {
+		string email = emailInput.text;
+		var regexPattern = @"^(([\w-]+\.)+[\w-]+|([a-zA-Z]{1}|[\w-]{2,}))@"
+                + @"((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\."
+                + @"([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\.([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                + @"([a-zA-Z]+[\w-]+\.)+[a-zA-Z]{2,4})$";
+
+		if (email != "" && Regex.IsMatch(email, regexPattern)) {
+			ToggleButtonStates (true);
+		} else {
+			ToggleButtonStates (false);
+		}
+	}
+
+	// Firebase methods
+	public void OnSignUp() {
+		authManager.SignUpNewUser(emailInput.text, passwordInput.text);
+		Debug.Log ("Sign Up");
+	}
+
+	public void OnLogin() {
+		authManager.LogInExistingUser(emailInput.text, passwordInput.text);
+		Debug.Log ("Login");
+	}
+
+	IEnumerator HandleAuthCallback(Task<Firebase.Auth.FirebaseUser> task, string operation)
+	{
+		if (task.IsCanceled)
+		{
+			UpdateStatus(operation + " was canceled.");
+		}
+		if (task.IsFaulted)
+		{
+			UpdateStatus(operation + " encountered an error: " + task.Exception);
+		} else if (task.IsCompleted)
+		{
+			// Firebase user has been created.
+			Firebase.Auth.FirebaseUser newUser = task.Result;
+			UpdateStatus("Loading game scene...");
+			yield return new WaitForSeconds(1.5f);
+			SceneManager.LoadScene("Player List");
+
+		}
+	}
+
+	private void OnDestroy()
+	{
+		authManager.authCallback -= HandleAuthCallback;
+	}
+
+	// Utilities
+	private void ToggleButtonStates(bool toState) {
+		signUpButton.interactable = toState;
+		loginButton.interactable = toState;
+	}
+
+	private void UpdateStatus(string message) {
+		statusText.text = message;
+	}
+}
